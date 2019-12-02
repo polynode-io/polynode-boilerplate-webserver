e83a1317d8220bb11d355585906891a5b7a58922
Jorge Duarte Rodríguez <info@malagadev.com>
2019-12-02 10:43:56 +0100

    first revision

---
+++ RequestContext.js
@@ -0,0 +1,97 @@
+/*
+ * polynode-boilerplate-webserver
+ *
+ * Released under MIT license. Copyright 2019 Jorge Duarte Rodriguez <info@malagadev.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * $Id:$
+ *
+ * @flow
+ * @format
+ *
+ */
+
+const Constants = {
+  DEFAULT_SUCCESS_HTTP_STATUS_CODE: 200,
+  DEFAULT_ERROR_HTTP_STATUS_CODE: 500,
+};
+
+const RequestContext = function({
+  getRequest,
+  getResponse,
+  next,
+  getServerHandler,
+  routeOptions,
+  log,
+}: {
+  getRequest: () => () => void,
+  getResponse: () => () => void,
+  next: () => () => void,
+  getServerHandler: () => {},
+  routeOptions: { responseContentType?: string },
+}) {
+  log.debug('NEW REQ CONTEXT. ');
+  this.getRequest = getRequest;
+  this.getResponse = getResponse;
+  this.next = next;
+  this.log = log;
+
+  this.defaultRouteOptions = {};
+
+  this.getRouteOptions = () => ({ ...this.defaultRouteOptions, ...routeOptions });
+
+  this.resolve = (
+    outputObj: any,
+    httpStatusCode: number = Constants.DEFAULT_SUCCESS_HTTP_STATUS_CODE
+  ) => {
+    const contentType =
+      routeOptions.responseContentType || getServerHandler().getConfig().defaultOutputContentType;
+    return getServerHandler().sendResponse({
+      httpStatusCode,
+      outputObj,
+      responseContentType: contentType,
+      log: this.log,
+      res: this.getResponse(),
+    });
+  };
+
+  this.resolveWithError = (err: ApplicationErrorType) => {
+    console.log('resolveWithError');
+    const httpStatusCode = err.httpStatusCode || Constants.DEFAULT_ERROR_HTTP_STATUS_CODE;
+    const outputObj =
+      'safeDetails' in err && err.safeDetails != null ? err.safeDetails : { internalError: true };
+
+    return getServerHandler().sendResponse({
+      httpStatusCode,
+      outputObj,
+      responseContentType: getServerHandler().getConfig().defaultOutputContentType,
+      log: this.log,
+      res: this.getResponse(),
+    });
+  };
+
+  this.reject = (err: ApplicationErrorType) => this.next(err);
+
+  this.getServerDependencies = () => getServerHandler().getDepsContainer();
+
+  getServerHandler()
+    .getDepsContainer()
+    .enhanceRequestContext.call(this, getServerHandler);
+};
+
+module.exports = RequestContext;
